d70f4ec82dd5c696c8b10526e42df62ce9ad595e
triton <joao@tritao.eu>
2015-11-24 04:08:09 +0000

    Fixed os.isdir to work with Windows symbolic links.

--- src/host/os_isdir.c
+++ src/host/os_isdir.c
@@ -8,20 +8,33 @@
 #include <sys/stat.h>
 #include "premake.h"
 
+#ifdef _WIN32
+#include <Windows.h>
+#endif
 
 int os_isdir(lua_State* L)
 {
 	struct stat buf;
 	const char* path = luaL_checkstring(L, 1);
+	int attr;
 
 	/* empty path is equivalent to ".", must be true */
 	if (strlen(path) == 0)
 	{
 		lua_pushboolean(L, 1);
 	}
+#ifdef _WIN32
+	// Use Windows-specific GetFileAttributes since it deals with symbolic links.
+	else if ((attr = GetFileAttributes(path)) != INVALID_FILE_ATTRIBUTES)
+	{
+		int isdir = (attr & FILE_ATTRIBUTE_DIRECTORY) != 0;
+		lua_pushboolean(L, (attr & FILE_ATTRIBUTE_DIRECTORY) != 0);
+	}
+#endif
 	else if (stat(path, &buf) == 0)
 	{
-		lua_pushboolean(L, buf.st_mode & S_IFDIR);
+		int isdir = (buf.st_mode & S_IFDIR) != 0;
+		lua_pushboolean(L, isdir);
 	}
 	else
 	{
